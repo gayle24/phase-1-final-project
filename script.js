f1498b315e034471067ff5e9afce18428c5393bd
Abigail Amani Wanjiku <abigail.wanjiku@student.moringaschool.com>
2023-07-08 04:02:36 +0300

    creating an appointment booking section

--- script.js
+++ script.js
@@ -1,3 +1,45 @@
+// const renderAppointmentForm = () => {
+//     display.innerHTML = '';
+  
+//     let form = document.createElement('div');
+//     form.setAttribute('class', 'appointment-form');
+  
+//     let title = document.createElement('h4');
+//     title.textContent = 'Book an Appointment';
+//     form.appendChild(title);
+  
+//     let descriptionLabel = document.createElement('label');
+//     descriptionLabel.textContent = 'Emergency Description:';
+//     form.appendChild(descriptionLabel);
+  
+//     let descriptionInput = document.createElement('textarea');
+//     descriptionInput.setAttribute('id', 'emergency-description');
+//     form.appendChild(descriptionInput);
+  
+//     let urgencyLabel = document.createElement('label');
+//     urgencyLabel.textContent = 'Urgency:';
+//     form.appendChild(urgencyLabel);
+  
+//     let urgencyDropdown = document.createElement('select');
+//     urgencyDropdown.setAttribute('id', 'urgency-dropdown');
+  
+//     let optionUrgent = document.createElement('option');
+//     optionUrgent.textContent = 'Urgent';
+//     urgencyDropdown.appendChild(optionUrgent);
+  
+//     let optionMinor = document.createElement('option');
+//     optionMinor.textContent = 'Minor';
+//     urgencyDropdown.appendChild(optionMinor);
+  
+//     form.appendChild(urgencyDropdown);
+  
+//     let submitButton = document.createElement('button');
+//     submitButton.textContent = 'Submit';
+//     form.appendChild(submitButton);
+  
+//     display.appendChild(form);
+//   };
+
 document.addEventListener('DOMContentLoaded', () => {
     let display = document.getElementById('display');
     let review = document.getElementById('reviews');
@@ -10,6 +52,7 @@ document.addEventListener('DOMContentLoaded', () => {
     .then(data => {
         data.forEach(data => {
             let list = document.createElement('div')
+            
             list.setAttribute('class', "list")
 
             let name = document.createElement('p');
@@ -24,6 +67,10 @@ document.addEventListener('DOMContentLoaded', () => {
             let bedcap = document.createElement('p');
             bedcap.textContent = `Available bed capacity: ${data.bedCapacity}`;
             list.appendChild(bedcap);
+            let book = document.createElement('input');
+            book.setAttribute("type", "submit");
+            book.setAttribute("value", "Book");
+            list.appendChild(book);
 
             display.appendChild(list)
         });
@@ -100,7 +147,5 @@ document.addEventListener('DOMContentLoaded', () => {
 
         renderData(data);
     });
-});
-
-     
+});   
     
